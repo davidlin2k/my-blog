851c3620f2797ce3683d4ad484ebd07b959d1a25
David Lin <davidzylin@outlook.com>
2023-10-21 23:39:41 -0400

    Added login and register routes

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<div>
+    <form method="POST">
+        <input type="text" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
+
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</div>
---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<div>
+    <h1>Settings</h1>
+</div>
