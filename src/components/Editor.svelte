f3ed2aa632f74068d39c7c0e6e6f10ab15f27ebd
David Lin <davidzylin@outlook.com>
2023-10-24 18:13:54 -0400

    Disable notification

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,33 +1,43 @@
-<script lang="ts">
+<script>
     import { onMount } from 'svelte';
     import { createEventDispatcher } from 'svelte';
 
     const dispatch = createEventDispatcher();
 
-    export let readOnly: boolean = false;
-    export let data: any = {};
+    export let readOnly = false;
+    export let data;
 
-    let editor: any;
+    let editor;
 
     onMount(async () => {
         const EditorJS = await import('@editorjs/editorjs');
         const Header = await import('@editorjs/header');
+        const ImageTool = await import('@editorjs/image');
 
         editor = new EditorJS.default({
             holder: 'editor',
             placeholder: 'Type something...',
             readOnly: readOnly,
             tools: {
-                header: Header.default
+                header: Header.default,
+                image: {
+                    class: ImageTool.default,
+                    config: {
+                        endpoints: {
+                            byFile: '/api/upload/image',
+                            byUrl: '/api/upload/image',
+                        },
+                    }
+                }
             },
             data: data
         });
     });
 
     export const save = () => {
-        editor.save().then((outputData: any) => {
+        editor.save().then((outputData) => {
             dispatch('save', outputData);
-        }).catch((error: any) => {
+        }).catch((error) => {
             console.log('Saving failed: ', error)
         });
     }
