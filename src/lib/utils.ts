f202e34645cbdf05cfd9feca37887793e97090ae
David Lin <davidzylin@outlook.com>
2023-10-24 19:21:10 -0400

    Added status

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,7 @@ export const formatDate = (date: string) => {
     const dateObj = new Date(date);
     return dateObj.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
 }
+
+export const capitalizeFirstLetter = (string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
